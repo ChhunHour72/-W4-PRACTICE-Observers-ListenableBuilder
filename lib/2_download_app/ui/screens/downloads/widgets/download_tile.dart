47b0156942a169267eb513f58dee30a1f91d5f54
Chhun Hour <longchhunhour74@gmail.com>
2026-02-22 22:42:23 +0700

    TASK-002  Downloader  Downloads - Q1  Create the download tiles - Handle the Icons

--- lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
+++ lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
@@ -14,11 +14,23 @@ class DownloadTile extends StatelessWidget {
     return ListenableBuilder(
       listenable: controller,
       builder: (context, child) {
-        
+        Widget trailingIcon = const SizedBox(); 
+
+        if (controller.status == DownloadStatus.notDownloaded) {
+          trailingIcon = IconButton(
+            icon: const Icon(Icons.download),
+            onPressed: controller.startDownload, 
+          );
+        } else if (controller.status == DownloadStatus.downloading) {
+          trailingIcon = const Icon(Icons.downloading);
+        } else if (controller.status == DownloadStatus.downloaded) {
+          trailingIcon = const Icon(Icons.folder);
+        }
+
         return Card(
           child: ListTile(
             title: Text(controller.ressource.name),
-
+            trailing: trailingIcon,
           ),
         );
       },
